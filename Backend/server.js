5c0595ba44ff09d38bd7c00d3376e81222a9e63c
Augustine Gyamprah <gyamprahaugustine07@gmail.com>
2026-04-04 22:58:04 +0100

    deploying

--- Backend/server.js
+++ Backend/server.js
@@ -38,6 +38,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 3000;
 
 const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || "http://localhost:3000";
